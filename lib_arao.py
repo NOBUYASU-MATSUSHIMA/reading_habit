ae75b867974446c097a748489ca160b6b92ebb5f
LAPTOP-M8I3GS8E\frontier-Python <Nobuyasu Matsutsushima / robroy.transition418@gmail.com>
2024-04-05 15:28:57 +0900

    荒尾図書館から福岡県立図書館へ差し替え

--- lib_arao.py
+++ lib_arao.py
@@ -8,34 +8,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-
-driver = webdriver.Chrome()
-
-def BookSearch_in_AraoLib():
-    url="https://www.arao-lib.jp/"
+def BookSearch_in_FukPrefLib():
+    driver = webdriver.Chrome()
+    url="https://www2.lib.pref.fukuoka.jp/"
     driver.get(url)
     driver.maximize_window()
     sleep(1)
     
-    driver.find_element(By.CSS_SELECTOR, "a:nth-child(2) > img").click()
-    sleep(1)
-    
-    driver.find_element(By.ID, "keyword").click()
-    sleep(1)
-    
-    driver.find_element(By.ID, "keyword").send_keys("python")
-    sleep(1)
-    
-    driver.find_element(By.CSS_SELECTOR, ".search").click()
-    sleep(1)
-    
-    driver.find_element(By.CSS_SELECTOR, ".pagerBox:nth-child(2) .disp:nth-child(2) > a").click()
-    sleep(1)
-     
-    driver.find_element(By.ID, "img_9784299008152").click()
-    sleep(3)
+    driver.set_window_size(1296, 688)
+    driver.find_element(By.CSS_SELECTOR, ".global_link:nth-child(2) .accordion-002-span").click()
+    driver.find_element(By.NAME, "txt_word").click()
+    driver.find_element(By.NAME, "txt_word").send_keys("Python")
+    driver.find_element(By.NAME, "submit_btn_searchEasy").click()
+    driver.find_element(By.CSS_SELECTOR, ".nav-area:nth-child(10) li:nth-child(2) > a").click()
+    driver.find_element(By.CSS_SELECTOR, "li:nth-child(2) .title").click()
 
     driver.quit()
+
+BookSearch_in_FukPrefLib()
 
 
 
